92eda5b1799da30025107732de87044095885272
yexiaohuaa <146563569+yexiaohuaa@users.noreply.github.com>
2023-11-30 13:18:17 +0800

    Create move.h

---
+++ move.h
@@ -0,0 +1,13 @@
+#ifndef move_h
+#define move_h
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+bool move(vector<char> &, int, vector<char> &, vector<char> (&)[10], vector<char> &, int);
+
+#endif /* move_h */
