e774cad1a2743d6b3b26e4bfb936f2d005528b4a
Weiqiao Chen <150210736+amandajochen@users.noreply.github.com>
2023-11-30 13:20:22 +0800

    Update deal_hide.cpp

--- deal_hide.cpp
+++ deal_hide.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 void deal_hide(vector<char>& left, vector<char>& source_pile, vector<char> (&hidden)[10], int source_col){
-  // Draw a card from the left and deal it to the source column of the deck
+  // Draw a card from the vector left and add it to the vector source_pile of source column pile
   char card = left.back();
   left.pop_back();
   source_pile.push_back(card);
-  // Since this hidden card is now opened, delete the dot representing the hidden card on the gameboard
+  // Since this hidden card is now uncovered, delete the dot representing the hidden card on the gameboard
   hidden[source_col].pop_back();
 }
