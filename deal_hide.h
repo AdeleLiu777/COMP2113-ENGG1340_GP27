6225e840b2806526889d569dad56e170fb78dd76
Weiqiao Chen <150210736+amandajochen@users.noreply.github.com>
2023-11-30 11:54:54 +0800

    Create deal_hide.h

---
+++ deal_hide.h
@@ -0,0 +1,11 @@
+#ifndef deal_hide_h
+#define deal_hide_h
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void deal_hide(vector<char> &, vector<char> &, vector<char> (&)[10], int);
+
+#endif /* board_h */
