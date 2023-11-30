703f3ba686548953312cae831d1b5b9890a2e26f
Weiqiao Chen <150210736+amandajochen@users.noreply.github.com>
2023-11-30 11:52:47 +0800

    Create board.h

---
+++ board.h
@@ -0,0 +1,11 @@
+#ifndef board_h
+#define board_h
+
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+void board(vector<char> [], vector<char> [], int &, int);
+
+#endif /* board_hpp */
